a1bae780a67a61e7ba7ade2b59f9bb7464b34a06
Saurabh Gupta <saurabhgupta20072005@gmail.com>
2026-06-24 17:22:09 +0530

    final fix

--- api/ml_utils.py
+++ api/ml_utils.py
@@ -1,9 +1,10 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+
 
 vectorizer = TfidfVectorizer()
 
 def match_score(resume_text, job_description):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.metrics.pairwise import cosine_similarity
     docs = [resume_text, job_description]
 
     tfidf_matrix = vectorizer.fit_transform(docs)
